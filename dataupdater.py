7a12053814459364ab690001e9de90cf93812ffb
AlanguidMan <169988300+AlanguidMan@users.noreply.github.com>
2024-05-16 21:30:36 +0530

    Update dataupdater.py

--- dataupdater.py
+++ dataupdater.py
@@ -102,13 +102,11 @@ else:
 sender = 'tradersbardataupdater@outlook.in'
 recipient = "papoye8837@nweal.com"
 message = "Hello world!"
-
 email = EmailMessage()
 email["From"] = sender
 email["To"] = recipient
 email["Subject"] = "Test Email"
 email.set_content(message)
-
 smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
 smtp.starttls()
 smtp.login(sender, "TradersbarStockMarket")
