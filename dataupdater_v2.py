4b6597f8ac3282c8a0e29e2a8f9559eb13eae393
AlanguidMan <169988300+AlanguidMan@users.noreply.github.com>
2024-05-20 18:58:45 +0530

    Major update with data fix

--- dataupdater_v2.py
+++ dataupdater_v2.py
@@ -36,9 +36,10 @@ holidays2024 = {
 }
 
 #current_date = datetime.datetime.now().strftime("%d-%m-%Y")
-current_date = "18-05-2024"
+current_date = "15-08-2024"
 print(current_date)
-
+csvfile= f'{current_date}.csv'
+pdffile= f'{current_date}.pdf'
 TelegramBotCredential2 = '6794059157:AAHHpVzTEl-oVNewNbLHJUoe6elTwqm7n5U'
 #TelegramBotCredential2 = os.environ.get("TelegramBotCredential2")
 #ReceiverTelegramID = os.environ.get("ReceiverTelegramID")
@@ -74,6 +75,37 @@ def format_as_crores(x):
     else:
         return x
 
+def mailSend():
+    start_time = time.time()
+    sender_email = "tradersbardataupdater@outlook.in"
+    recipient_email = "dotitebaj.jitavudon@rungel.net"
+    subject = f"Delivery position for {current_date} "
+    password = "TradersBarStockMarket"
+    recipient_email = "vamiy71000@mcatag.com"
+    msg = MIMEMultipart()
+    msg["From"] = sender_email
+    msg["To"] = recipient_email
+    msg["Subject"] = subject
+    msg.preamble = subject
+    f= open(csvfile)
+    attachment = MIMEText(f.read(), _subtype="csv")
+    f.close
+    attachment.add_header("Content-Disposition", "attachment", filename=f"{csvfile}")
+    msg.attach(attachment)
+    with open(pdffile, 'rb') as f:
+        attachment = MIMEApplication(f.read(), _subtype="pdf")
+        attachment.add_header('Content-Disposition','attachment',filename=f"{pdffile}")
+        msg.attach(attachment)
+    server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+    server.starttls()
+    server.login(sender_email, password)
+    print(f"logged in {password}@{sender_email}")
+    server.sendmail(sender_email, recipient_email, msg.as_string())
+    server.quit()
+    end_time = time.time()
+    exim = end_time - start_time
+    print("email sent in: ", exim)
+    
 
 my_list = [
     "Niftybees", "bankbees", "hdfcsensex",
@@ -93,18 +125,22 @@ my_list = [
 def security_wise_archive(from_date, to_date, symbol, drop, series="ALL"):
     base_url = "https://www.nseindia.com/api/historical/securityArchives"
     url = f"{base_url}?from={from_date}&to={to_date}&symbol={symbol.upper()}&dataType=priceVolumeDeliverable&series={series.upper()}"
+    #print(url)
     p = nsefetch(url)
     df = pd.DataFrame(p['data'])
-    selected_columns = ['mTIMESTAMP','CH_SYMBOL','CH_TOT_TRADED_QTY','CH_TOT_TRADED_VAL','COP_DELIV_QTY','COP_DELIV_PERC']
-    df[['CH_TOT_TRADED_VAL']] = df[['CH_TOT_TRADED_VAL']].applymap(format_as_crores)
-    df[["CH_TOT_TRADED_QTY", "COP_DELIV_QTY"]] = df[["CH_TOT_TRADED_QTY", "COP_DELIV_QTY"]].applymap(format_number)
-    # Create new DataFrame from selected columns
-    new_df = df[selected_columns]
-    headers = ['Date', 'Symbol', 'Total Traded Qty', 'Total Traded Value', 'Total Delivery Quantity', 'Delivery Percentage']
-    new_df.columns = headers
-    return(new_df)
-    #final_df = pd.DataFrame()
-    #final_df = final_df.append(new_df)
+    if df.empty:
+        return None
+    else:
+        #return f"received data for {symbol}."
+        selected_columns = ['mTIMESTAMP','CH_SYMBOL','CH_TOT_TRADED_QTY','CH_TOT_TRADED_VAL','COP_DELIV_QTY','COP_DELIV_PERC']
+        df[['CH_TOT_TRADED_VAL']] = df[['CH_TOT_TRADED_VAL']].applymap(format_as_crores)
+        df[["CH_TOT_TRADED_QTY", "COP_DELIV_QTY"]] = df[["CH_TOT_TRADED_QTY", "COP_DELIV_QTY"]].applymap(format_number)
+        # Create new DataFrame from selected columns
+        new_df = df[selected_columns]
+        headers = ['Date', 'Symbol', 'Total Traded Qty', 'Total Traded Value', 'Total Delivery Quantity', 'Delivery Percentage']
+        new_df.columns = headers
+        return(new_df)
+
 """
     # Write to CSV file in append mode
     if drop:
@@ -121,25 +157,34 @@ print("cancating rdx to dfx ")
 print(result)
 """
 start_time = time.time()
+startime = start_time
 rdx = pd.DataFrame()
+
 for i in my_list:
-    #print("Rdx is empty:",rdx.empty)
     dfx= security_wise_archive(current_date,current_date,i,drop = False)
-    #print(dfx.info())
-    #print(type(dfx))
-    #print(type(rdx))
-    if not dfx.empty:
-        rdx = pd.concat([rdx, dfx], ignore_index=False)
-        #print(rdx)
+    if dfx is None:
+        break
     else:
-        print(f"DataFrame {i} is empty")
-print(rdx)
-html_table = rdx.to_html()
-subprocess.run(['sudo','apt-get', 'install', 'wkhtmltopdf'])
-pdfkit.from_string(html_table, f'{current_date}.pdf')
-end_time = time.time()
-exe_time = end_time - start_time
-print("Data fetched and converted in: ", exe_time)
+        rdx = pd.concat([rdx, dfx], ignore_index=False)
+
+if not rdx.empty:
+    rdx.to_csv(f"{current_date}.csv")
+    html_table = rdx.to_html()
+    subprocess.run(['sudo','apt-get', 'install', 'wkhtmltopdf'])
+    pdfkit.from_string(html_table, f'{current_date}.pdf')
+    end_time = time.time()
+    exe_time = end_time - start_time
+    print("Data fetched and converted in: ", exe_time)
+    mailSend()
+    SendTelegramFile(csvfile)
+    SendTelegramFile(pdffile)
+    final_end_time = time.time()
+    totime = final_end_time-startime
+    print("Code executed in:", totime)
+    SendMessageToTelegram(f"Execution time: {totime} seconds")
+
+
+
 """
 for counter,i in enumerate(my_list):
     if counter == 0:
@@ -148,7 +193,6 @@ for counter,i in enumerate(my_list):
        security_wise_archive(current_date, current_date,i,drop=True)
 
 #Multi threading
-
 
 import concurrent.futures
 import time
@@ -171,49 +215,8 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
             rdx = pd.concat([rdx, dfx], ignore_index=False)
 
 print(rdx)
+
 end_time = time.time()
 execution_time = end_time - start_time
 print(f"Execution time: {execution_time} seconds")
 """
-start_time = time.time()
-sender_email = "tradersbardataupdater@outlook.in"
-recipient_email = "dotitebaj.jitavudon@rungel.net"
-subject = f"Delivery position for {current_date} "
-password = "TradersBarStockMarket"
-recipient_email = "vamiy71000@mcatag.com"
-msg = MIMEMultipart()
-msg["From"] = sender_email
-msg["To"] = recipient_email
-msg["Subject"] = subject 
-msg.preamble = subject
-csvfile= f'{current_date}.csv'
-pdffile= f'{current_date}.pdf'
-"""
-fp = open(fileToSend)
-# Note: we should handle calculating the charset
-attachment = MIMEText(fp.read(), _subtype=subtype)
-fp.close()
-attachment.add_header("Content-Disposition", "attachment", filename=fileToSend)
-msg.attach(attachment)
-"""
-f= open(csvfile)
-attachment = MIMEText(f.read(), _subtype="csv")
-f.close
-attachment.add_header("Content-Disposition", "attachment", filename=f"{csvfile}")
-msg.attach(attachment)
-with open(pdffile, 'rb') as f:
-    attachment = MIMEApplication(f.read(), _subtype="pdf")
-    attachment.add_header('Content-Disposition','attachment',filename=f"{pdffile}")
-    msg.attach(attachment)
-server = smtplib.SMTP("smtp-mail.outlook.com", 587)
-server.starttls()
-server.login(sender_email, password)
-print(f"logged in {password}@{sender_email}")
-server.sendmail(sender_email, recipient_email, msg.as_string())
-server.quit()
-SendTelegramFile(csvfile)
-SendTelegramFile(pdffile)
-end_time = time.time()
-exim = end_time - start_time
-print("email sent in: ", exim)
-#SendMessageToTelegram(f"Execution time: {execution_time} seconds")
