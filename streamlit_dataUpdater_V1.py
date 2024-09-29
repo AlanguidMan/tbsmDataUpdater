6a10eefae8963cc6742117d0c0a6e9f18bab2c33
AlanguidMan <169988300+AlanguidMan@users.noreply.github.com>
2024-09-29 11:02:39 +0530

    Create streamlit_dataUpdater_V1.py

---
+++ streamlit_dataUpdater_V1.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+import os
+from nsepython import nsefetch  # Ensure you have this package installed
+import requests
+
+# List of symbols
+my_list = [
+    "Niftybees", "niftyietf", "setfnif50", "bankbees",
+    "bankietf", "hdfcsensex", "nv20ietf", "juniorbees",
+    "monifty500", "midcap", "midcapetf", "hdfcsml250",
+    "pvtbanietf", "psubnkbees", "bfsi", "cpseetf",
+    "icicib22", "autobees", "itbees", "itietf",
+    "tnidetf", "pharmabees", "healthy", "infraietf",
+    "makeindia", "consumbees", "Commoietf", "Lowvolietf",
+    "mom30ietf", "smallcap", "alpha", "goldbees",
+    "silverbees", "mon100", "masptop50", "mafang",
+    "morealty", "ltgiltbees", "esg", "ALPL30IETF", "oilietf",
+    "metalietf", "modefence"
+]
+
+# Streamlit app setup
+st.title("Data Updater V1.0")
+
+# User input for dates
+from_date = st.date_input("From Date", datetime.today())
+to_date = st.date_input("To Date", datetime.today())
+
+# User selection for symbol
+selected_symbol = st.selectbox("Select a Symbol", my_list)
+
+# Function to fetch and format data
+def format_number(amount):
+    if amount >= 10000000:
+        return f"{amount / 10000000:.2f} crore"
+    elif amount >= 100000:
+        return f"{amount / 100000:.2f} lakh"
+    else:
+        return f"{amount / 1000:.2f} thousand"
+
+def format_as_crores(x):
+    if isinstance(x, float):
+        return f"{x / 10000000:.2f} Cr"
+    else:
+        return x
+
+def security_wise_archive(from_date, to_date, symbol, series="ALL"):
+    base_url = "https://www.nseindia.com/api/historical/securityArchives"
+    url = f"{base_url}?from={from_date}&to={to_date}&symbol={symbol.upper()}&dataType=priceVolumeDeliverable&series={series.upper()}"
+    p = nsefetch(url)
+    df = pd.DataFrame(p['data'])
+    if df.empty:
+        return None
+    else:
+        selected_columns = ['mTIMESTAMP', 'CH_SYMBOL', 'CH_TOT_TRADED_QTY', 'CH_TOT_TRADED_VAL', 'COP_DELIV_QTY', 'COP_DELIV_PERC']
+        df[['CH_TOT_TRADED_VAL']] = df[['CH_TOT_TRADED_VAL']].applymap(format_as_crores)
+        df[["CH_TOT_TRADED_QTY", "COP_DELIV_QTY"]] = df[["CH_TOT_TRADED_QTY", "COP_DELIV_QTY"]].applymap(format_number)
+        new_df = df[selected_columns]
+        headers = ['Date', 'Symbol', 'Total Traded Qty', 'Total Traded Value', 'Total Delivery Quantity', 'Delivery Percentage']
+        new_df.columns = headers
+        return new_df
