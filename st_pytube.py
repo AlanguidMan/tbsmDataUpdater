0d3931f506a5a43f55d35856262633efabd2d657
AlanguidMan <169988300+AlanguidMan@users.noreply.github.com>
2024-09-30 01:03:16 +0530

    Update st_pytube.py

--- st_pytube.py
+++ st_pytube.py
@@ -6,21 +6,24 @@ st.title("YouTube Downloader")
 youtube_url = st.text_input("Enter the YouTube URL")
 
 resolution = st.selectbox("Select the resolution", ["Highest Resolution", "720p", "480p", "360p", "240p", "144p"])
-format = st.selectbox("Select the format", ["MP4", "MP3"])
+file_format = st.selectbox("Select the format", ["MP4", "MP3"])
 
 if st.button("Download Video"):
     try:
         youtube_object = YouTube(youtube_url)
+        
         if resolution == "Highest Resolution":
-            if format == "MP4":
+            if file_format == "MP4":
                 youtube_object.streams.get_highest_resolution().download()
-            elif format == "MP3":
-                youtube_object.streams.filter(only_audio=True).first().download()
+            elif file_format == "MP3":
+                youtube_object.streams.filter(only_audio=True).first().download(filename_prefix="audio_")
         elif resolution in ["720p", "480p", "360p", "240p", "144p"]:
-            if format == "MP4":
-                youtube_object.streams.get_by_resolution(resolution).download()
-            elif format == "MP3":
-                youtube_object.streams.filter(only_audio=True, resolution=resolution).first().download()
+            if file_format == "MP4":
+                youtube_object.streams.filter(res=resolution, file_extension='mp4').first().download()
+            elif file_format == "MP3":
+                youtube_object.streams.filter(only_audio=True, abr='128kbps').first().download(filename_prefix="audio_")
+        
         st.success("Download completed successfully.")
     except Exception as e:
         st.error(f"An error occurred: {e}")
+        
